a92dd2f8b57b7ab356ae302efccdeb309badf352
rcnerd <hunter.g.wells@gmail.com>
2015-06-19 20:52:02 -0400

    fixed manual/auto switch and light algorithms

--- solartrackerdraftwithlights/solartrackerdraftwithlights.ino
+++ solartrackerdraftwithlights/solartrackerdraftwithlights.ino
@@ -101,6 +101,13 @@ void loop()
       tracked();
     }
 
+    // if manual switch set to on turn on
+    if (digitalRead(manswitch) == 1)
+    {
+      Serial.println("manual lights on");
+      digitalWrite(switchpin, HIGH);
+    }
+
     // if night turn on lights
     //take light value
     lightval = (analogRead(0) + analogRead(1)) / 2;
@@ -109,14 +116,16 @@ void loop()
       high = 600;
       low = 600;
 
-      panel.write(5);
+      pos = 5;
+      panel.write(pos);
 
-      Serial.println("Night: lights on.");
+      Serial.println(pos);
 
       // if switch is set to manual off dont turn on light
       if (digitalRead(autoon) == 1)
       {
         digitalWrite(switchpin, HIGH);
+        Serial.println("Night: lights on.");
       }
       for (int time = 30000; time > 0; time = time / 1.1)
       {
@@ -151,126 +160,121 @@ void loop()
         digitalWrite(switchpin, LOW);
       }
 
-      // if manual switch set to on turn on
-      if (digitalRead(manswitch) == 1)
-      {
-        digitalWrite(switchpin, HIGH);
-        {
+    }
 
-          // only turn off if manual switch is turned to off or if manual is on and it is day (see above) turn off
-          if (digitalRead(manswitch) == 0 && digitalRead(autoon) == 0)
-          {
-            digitalWrite(switchpin, LOW);
-          }
+    // only turn off if manual switch is turned to off or if auto is on and it is day (see above) turn off
+    if (digitalRead(manswitch) == 0 && digitalRead(autoon) == 0)
+    {
+      digitalWrite(switchpin, LOW);
+    }
 
-        }
-        // if voltage less than good wait 1 min
-      }
+    // if voltage less than good wait 1 min
+  }
+
+
+  else
+  {
+    Serial.println("low voltage");
+    //delay(15000);
+  }
+
+}
+
+
+
+void autoTune()
+{
+  // update latest low and high values
+  if (lightLevel < low)
+  {
+    low = lightLevel;
+  }
+
+  if (lightLevel1 < low)
+  {
+    low = lightLevel1;
+  }
+
+
+
+  if (lightLevel > high)
+  {
+    high = lightLevel;
+  }
+
+  if (lightLevel1 > high)
+  {
+    high = lightLevel1;
+  }
+
+  // dilate down then up to avoid twitching then trim
+  lightLevel = map(lightLevel, low + 1, high - 1, 0, precision);
+  lightLevel = map(lightLevel, 0, precision, 0, 60);
+  lightLevel = constrain(lightLevel, 0, 60);
+
+  lightLevel1 = map(lightLevel1, low + 1, high - 1, 0, precision);
+  lightLevel1 = map(lightLevel1, 0, precision, 0, 60);
+  lightLevel1 = constrain(lightLevel1, 0, 60);
+}
+
+void tracked()
+{
+  Serial.println("paused");
+  for (int secs = 600; secs > 0; secs -= 1)
+  {
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(100);
+    int secs22;
+    if (secs22 == 1)
+    {
+
+      secs22++;
+    }
+    else if ( secs22 == 2 || secs22 == 3 || secs22 == 4 || secs22 == 5 || secs22 == 6 || secs22 == 7 || secs22 == 8 || secs22 == 9)
+    {
+      secs22++;
+    }
+    else
+    {
+      //Serial.println((analogRead(0)+analogRead(1))/2);
+      Serial.println(secs);
+      secs22 = 1;
+    }
+
+    if (Serial.available() > 0) {
+
+      // say what you got:
+      Serial.print("I received a message:   ");
+      Serial.println(Serial.readString());
+
+      Serial.println("Retracking");
+      secs = 0;
+
     }
   }
-      else
-      {
-        Serial.println("low voltage");
-        //delay(15000);
-      }
+}
 
-    }
-  
+void track()
+{
+  // read photoRs
+  lightLevel = analogRead(sensorPin1);
+  lightLevel1 = analogRead(sensorPin);
 
+  //Serial.println((analogRead(0)+analogRead(1))/2);
 
-    void autoTune()
-    {
-      // update latest low and high values
-      if (lightLevel < low)
-      {
-        low = lightLevel;
-      }
+  //map and constrain values to reacent high and low
+  autoTune();
 
-      if (lightLevel1 < low)
-      {
-        low = lightLevel1;
-      }
+  // calculate new position
+  pos = pos + trakspeed * (lightLevel - lightLevel1);
 
+  pos = pos - 2;
+  //Serial.println("-");
+  Serial.println(pos);
 
-
-      if (lightLevel > high)
-      {
-        high = lightLevel;
-      }
-
-      if (lightLevel1 > high)
-      {
-        high = lightLevel1;
-      }
-
-      // dilate down then up to avoid twitching then trim
-      lightLevel = map(lightLevel, low + 1, high - 1, 0, precision);
-      lightLevel = map(lightLevel, 0, precision, 0, 60);
-      lightLevel = constrain(lightLevel, 0, 60);
-
-      lightLevel1 = map(lightLevel1, low + 1, high - 1, 0, precision);
-      lightLevel1 = map(lightLevel1, 0, precision, 0, 60);
-      lightLevel1 = constrain(lightLevel1, 0, 60);
-    }
-  
-    void tracked()
-    {
-      Serial.println("paused");
-      for (int secs = 600; secs > 0; secs -= 1)
-      {
-        digitalWrite(13, HIGH);
-        delay(100);
-        digitalWrite(13, LOW);
-        delay(100);
-        int secs22;
-        if (secs22 == 1)
-        {
-
-          secs22++;
-        }
-        else if ( secs22 == 2 || secs22 == 3 || secs22 == 4 || secs22 == 5 || secs22 == 6 || secs22 == 7 || secs22 == 8 || secs22 == 9)
-        {
-          secs22++;
-        }
-        else
-        {
-          //Serial.println((analogRead(0)+analogRead(1))/2);
-          Serial.println(secs);
-          secs22 = 1;
-        }
-
-        if (Serial.available() > 0) {
-
-          // say what you got:
-          Serial.print("I received a message:   ");
-          Serial.println(Serial.readString());
-
-          Serial.println("Retracking");
-          secs = 0;
-
-        }
-      }
-    }
-
-    void track()
-    {
-      // read photoRs
-      lightLevel = analogRead(sensorPin1);
-      lightLevel1 = analogRead(sensorPin);
-
-      //Serial.println((analogRead(0)+analogRead(1))/2);
-
-      //map and constrain values to reacent high and low
-      autoTune();
-
-      // calculate new position
-      pos = pos + trakspeed * (lightLevel - lightLevel1);
-
-      pos = pos - 2;
-      //Serial.println("-");
-      Serial.println(pos);
-
-      pos = constrain(pos, 0, 180);
-      delay(125);
-    }
+  pos = constrain(pos, 0, 180);
+  delay(125);
+}
 
